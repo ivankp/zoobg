3362847a32fb88863e345adccc3a5833b7464de2
Ivan Pogrebnyak <ivan.pogrebnyak@gmail.com>
2016-02-03 21:55:19 -0500

    flip dice for 1 move

--- gnuzoo.py
+++ gnuzoo.py
@@ -205,6 +205,13 @@ class hint:
             if (self.moves[0][0]-self.moves[0][1]>dice[0]) \
             or (self.moves[1][0]-self.moves[1][1]>dice[1]):
                 self.dice[0], self.dice[1] = self.dice[1], self.dice[0]
+        elif len(self.moves)==1:
+            if self.moves[0][1]==0:
+                if self.moves[0][0]-self.moves[0][1]<dice[0]:
+                    self.dice[0], self.dice[1] = self.dice[1], self.dice[0]
+            else:
+                if self.moves[0][0]-self.moves[0][1]!=dice[0]:
+                    self.dice[0], self.dice[1] = self.dice[1], self.dice[0]
         elif len(self.moves)>2:
             i = 0
             while i<len(self.moves)-1:
