df3ae6e05bc602a68232ee3ea349735d692ca8a8
Ivan Pogrebnyak <ivan.pogrebnyak@gmail.com>
2016-02-08 02:10:16 -0500

    logfile write forgot self

--- logfile.py
+++ logfile.py
@@ -3,7 +3,7 @@ import datetime
 class logfile:
     def __init__(self,fname):
         self.fname = fname
-    def write(s):
+    def write(self,s):
         with open(self.fname,'a') as f:
             f.write(datetime.datetime.now().\
               strftime("%Y-%m-%d %H:%M:%S")+' '+s+'\n')
